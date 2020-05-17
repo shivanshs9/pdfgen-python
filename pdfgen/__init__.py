6da509b952df347d66ea924f5d149399e7aca687
Shivansh Saini <shivanshs9@gmail.com>
2020-05-17 23:47:58 +0530

    Version bump -> 1.0.2
    - Include readme in poetry

--- pdfgen/__init__.py
+++ pdfgen/__init__.py
@@ -5,7 +5,7 @@ Pyppeteer-based async python wrapper to convert html to pdf
 
 __forked_from__ = 'Golovanov Stanislav'
 __author__ = 'Shivansh Saini'
-__version__ = '1.0.0'
+__version__ = '1.0.2'
 __license__ = 'MIT'
 
 import pdfgen.api_sync as sync
